ba687d68c895fafa2ab0070a52eabebd68e80cd3
GabbyGreat <gabrieloranekwu@gmail.com>
2024-01-18 22:29:02 +0100

    :boom: Did the clip thing

--- lib/module/sample/main.dart
+++ lib/module/sample/main.dart
@@ -1,133 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-
-class DialogBody extends StatefulWidget {
-  const DialogBody({super.key});
-
-  @override
-  State<DialogBody> createState() => _DialogBodyState();
-}
-
-class _DialogBodyState extends State<DialogBody> {
-  double circleRadius = 40;
-
-  @override
-  Widget build(BuildContext context) {
-    var boxHeight = MediaQuery.sizeOf(context).height * 0.4;
-    return BackdropFilter(
-      filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-      child: Center(
-        child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 30),
-          //we can set a height of our choice
-          height: boxHeight,
-          child: Stack(
-            children: [
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: FractionallySizedBox(
-                  //90% of the whatever height we choose
-                  heightFactor: 0.9,
-                  child: ClipPath(
-                    clipper: CurvedPath(
-                      circleDiameter: boxHeight * 0.2,
-                    ),
-                    child: Container(
-                      color: const Color.fromRGBO(16, 52, 165, 1),
-                    ),
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.topCenter,
-                child: FractionallySizedBox(
-                  // occupies 20% of that desired height
-                  heightFactor: 0.2,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color.fromRGBO(16, 52, 165, 1),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class CurvedPath extends CustomClipper<Path> {
-  final double circleDiameter;
-
-  CurvedPath({required this.circleDiameter});
-
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    double offset = 15;
-
-    path.moveTo(0, offset);
-    path.lineTo(0, size.height - offset);
-    path.quadraticBezierTo(0, size.height, offset, size.height);
-    path.lineTo(size.width - offset, size.height);
-    path.quadraticBezierTo(
-      size.width,
-      size.height,
-      size.width,
-      size.height - offset,
-    );
-    path.lineTo(size.width, offset);
-    path.quadraticBezierTo(
-      size.width,
-      0,
-      size.width - offset,
-      0,
-    );
-
-    /// Begin of the curve
-    /// I ignored the use of circleDiameter
-    /// It will be responsive regardless
-
-    path.lineTo((size.width / 2) + 60, 0);
-    path.cubicTo(
-      (size.width / 2) + 35, // you can adjust
-      0,
-      (size.width / 2) + 50, // you can adjust
-      40,
-      size.width / 2, // you can adjust
-      45,
-    );
-    path.cubicTo(
-      (size.width / 2) - 35, // you can adjust
-      45,
-      (size.width / 2) - 45, // you can adjust
-      0,
-      (size.width / 2) - 60, // you can adjust
-      0,
-    );
-
-    /// End of the curve
-
-    path.lineTo(offset, 0);
-    path.quadraticBezierTo(0, 0, 0, offset);
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
-}
-
-Future<T?> showAppDiolog<T>(BuildContext context) async {
-  return showDialog(
-    context: context,
-    builder: (context) => const DialogBody(),
-  );
-}
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SampleScreen extends StatefulWidget {
   const SampleScreen({super.key});
@@ -137,15 +11,78 @@ class SampleScreen extends StatefulWidget {
 }
 
 class _SampleScreenState extends State<SampleScreen> {
+  double circleRadius = 40;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => showAppDiolog(context),
-          child: const Text('Press'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CustomPaint(
+              size: const Size(200, 200),
+              painter: BoxedPaint(),
+            ),
+            20.verticalSpace,
+            ClipPath(
+              clipper: BoxedPath(),
+              child: Container(
+                color: Colors.red,
+                width: 200,
+                height: 200,
+              ),
+            )
+          ],
         ),
       ),
     );
   }
+}
+
+class BoxedPaint extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 5;
+
+    double offset = 30;
+
+    var path = Path();
+    path.moveTo(offset, 0);
+    path.lineTo(0, offset);
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
+
+class BoxedPath extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double offset = 30;
+
+    var path = Path();
+    path.moveTo(offset, 0);
+    path.lineTo(0, offset);
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
 }
